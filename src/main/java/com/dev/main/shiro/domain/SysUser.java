7569172bc03b8abf43a0eae9d17f9d24e2de39f6
溶酶菌 <hayatesa@live.cn>
2018-09-11 16:24:43 +0800

    +

---
+++ src/main/java/com/dev/main/shiro/domain/SysUser.java
@@ -0,0 +1,172 @@
+package com.dev.main.shiro.domain;
+
+import com.dev.main.common.domain.BaseDomain;
+
+import java.util.Date;
+
+public class SysUser extends BaseDomain {
+    // （管理）系统用户 主键 (自增)
+    private Long id;
+
+    // 用户名 唯一
+    private String username;
+
+    // 微信号
+    private String wechatId;
+
+    // 密码
+    private String password;
+
+    // 盐值
+    private String salt;
+
+    // 上次登录时间
+    private Date lastAccessTime;
+
+    // 状态：1-可用，0-禁用
+    private Byte status;
+
+    // 手机号
+    private String phone;
+
+    // 性别
+    private Byte gender;
+
+    // 姓名
+    private String name;
+
+    // 身份证号
+    private String idCard;
+
+    // 邮箱
+    private String email;
+
+    // 是否删除 1-删除
+    private Byte isDeleted;
+
+    // 创建时间
+    private Date gmtCreate;
+
+    // 修改时间
+    private Date gmtModified;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getWechatId() {
+        return wechatId;
+    }
+
+    public void setWechatId(String wechatId) {
+        this.wechatId = wechatId == null ? null : wechatId.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt == null ? null : salt.trim();
+    }
+
+    public Date getLastAccessTime() {
+        return lastAccessTime;
+    }
+
+    public void setLastAccessTime(Date lastAccessTime) {
+        this.lastAccessTime = lastAccessTime;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public Byte getGender() {
+        return gender;
+    }
+
+    public void setGender(Byte gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard == null ? null : idCard.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Byte getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Byte isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+}
