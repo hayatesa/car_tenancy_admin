800b25506cb18bf2689f791b4eb01b98502033bd
溶酶菌 <hayatesa@live.cn>
2018-09-20 09:35:27 +0800

    修改fastjson配置

--- src/main/java/com/dev/main/common/config/FastJsonConverterConfig.java
+++ src/main/java/com/dev/main/common/config/FastJsonConverterConfig.java
@@ -1,22 +1,31 @@
 package com.dev.main.common.config;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.Iterator;
 import java.util.List;
 
 @Configuration
-public class FastJsonConfig implements WebMvcConfigurer {
+public class FastJsonConverterConfig implements WebMvcConfigurer {
 
     // fastjson配置
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Iterator<HttpMessageConverter<?>> iterator = converters.iterator();
+        while(iterator.hasNext()){
+            HttpMessageConverter<?> converter = iterator.next();
+            if(converter instanceof MappingJackson2HttpMessageConverter){
+                iterator.remove();
+            }
+        }
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
-        com.alibaba.fastjson.support.config.FastJsonConfig config = new com.alibaba.fastjson.support.config.FastJsonConfig();
+        FastJsonConfig config = new FastJsonConfig();
         config.setSerializerFeatures(SerializerFeature.WriteNullListAsEmpty, // List类型字段为null时输出[]而非null
                 SerializerFeature.WriteMapNullValue, // 显示空字段
                 SerializerFeature.WriteNullStringAsEmpty, // 字符串类型字段为null时间输出""而非null
