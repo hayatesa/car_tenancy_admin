4ad93507612716550d2d81029d6eecf0a14c54c8
溶酶菌 <hayatesa@live.cn>
2018-09-21 15:42:01 +0800

    更新fastJsaon配置

--- src/main/java/com/dev/main/common/config/FastJsonConverterConfig.java
+++ src/main/java/com/dev/main/common/config/FastJsonConverterConfig.java
@@ -4,10 +4,13 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -33,9 +36,28 @@ public class FastJsonConverterConfig implements WebMvcConfigurer {
                 SerializerFeature.PrettyFormat, // 美化json输出，否则会作为整行输出
                 SerializerFeature.WriteNullNumberAsZero, // 数值字段如果为null,输出为0,而非null
                 SerializerFeature.WriteNullBooleanAsFalse, // Boolean字段如果为null,输出为false,而非null
-                SerializerFeature.WriteDateUseDateFormat,
+                SerializerFeature.WriteDateUseDateFormat, // 时间格式yyyy-MM-dd HH:mm:ss
                 SerializerFeature.DisableCircularReferenceDetect);  // 关闭循环引用
         converter.setFastJsonConfig(config);
+        List<MediaType> supportedMediaTypes = new ArrayList<>();
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+        supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+        supportedMediaTypes.add(MediaType.APPLICATION_PDF);
+        supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_XML);
+        supportedMediaTypes.add(MediaType.IMAGE_GIF);
+        supportedMediaTypes.add(MediaType.IMAGE_JPEG);
+        supportedMediaTypes.add(MediaType.IMAGE_PNG);
+        supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
+        supportedMediaTypes.add(MediaType.TEXT_HTML);
+        supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
+        supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+        supportedMediaTypes.add(MediaType.TEXT_XML);
+        converter.setSupportedMediaTypes(supportedMediaTypes);
         converters.add(converter);
     }
 
