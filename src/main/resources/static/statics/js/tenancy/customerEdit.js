496562e55c605c942e745173b47712188e1db20d
141411 <2414773483@qq.com>
2018-09-27 15:26:06 +0800

    kehuxinxiEdit.js

--- src/main/resources/static/statics/js/tenancy/customerEdit.js
+++ src/main/resources/static/statics/js/tenancy/customerEdit.js
@@ -4,22 +4,22 @@ var addressAreaId;
 var genderId;
 //data
 var customerData = {
-    customer: {
-        id: "",
-        name: "",
-        phone: "",
-        gender: "",
-        idCard: "",
-        email: "",
-        tncAddress: {
-            province: {id: "", name: ""},
-            city: {id: "", name: ""},
-            area: {id: "", name: ""},
-            detail: "",
+    customer:{
+        id:"",
+        name:"",
+        phone:"",
+        gender:"",
+        idCard:"",
+        email:"",
+        tncAddress:{
+            province:{id:"",name:""},
+            city:{id:"",name:""},
+            area:{id:"",name:""},
+            detail:"",
         },
-        emergencyName: "",
-        emergencyPhone: "",
-        password: "",
+        emergencyName:"",
+        emergencyPhone:"",
+        password:"",
     }
 };
 //Vue app，编辑页
@@ -39,22 +39,20 @@ $(document).ready(function () {
     $.ajax({
         type: "get",
         url: "/api/customer/edit",
-        data: {
+        data:{
             uid: uid
         },
-        success: function (res) {
-            if (res.code == 0) {
+        success:function(res) {
+            if(res.code == 0) {
                 customerEdit_app.customer = res.data;
-                console.log(customerEdit_app.customer);
-                console.log("ddddd");
-                genderId = "gender" + customerEdit_app.customer.gender;
-                if (customerEdit_app.customer.gender == 1) {
+                genderId = "gender"+customerEdit_app.customer.gender;
+                if(customerEdit_app.customer.gender==1) {
                     $($("#gender").siblings().eq(0).children("div").children("input")[0]).attr('value', "男");
-                } else {
+                } else{
                     $($("#gender").siblings().eq(0).children("div").children("input")[0]).attr('value', "女");
                 }
                 loadAddress(0, 0);
-                if (res.data.tncAddress != null) {
+                if(res.data.tncAddress!=null) {
                     var provinceName = res.data.tncAddress.province.name;
                     var cityName = res.data.tncAddress.city.name;
                     var areaName = res.data.tncAddress.area.name;
@@ -84,18 +82,18 @@ layui.use('form', function () {
     });
     form.verify({
         repeat: function (value, item) {
-            if ($("#password").val() != '' && $("#password").val() != $("#repeatPassword").val()) {
+            if($("#password").val()!='' && $("#password").val()!=$("#repeatPassword").val()) {
                 return "两次密码不一致";
                 //layer.msg("两次密码不一致", { time: 1500})
             }
         },
-        allAddress: function (value, item) {
+        allAddress:function (value, item) {
             var pAddress = $($("#province").siblings().eq(0).children("div").children("input")[0]).val();
             var cAddress = $($("#city").siblings().eq(0).children("div").children("input")[0]).val();
             var aAddress = $($("#area").siblings().eq(0).children("div").children("input")[0]).val();
             var dAddress = $("#detail").val();
-            if (!((pAddress != '' && cAddress != '' && aAddress != '' && dAddress != '')
-                || (pAddress == '' && cAddress == '' && aAddress == '' && dAddress == ''))) {
+            if(!((pAddress!=''&&cAddress!=''&&aAddress!=''&&dAddress!='')
+                ||(pAddress==''&&cAddress==''&&aAddress==''&&dAddress==''))) {
                 return "地址填写不完整";
             }
         }
@@ -105,20 +103,19 @@ layui.use('form', function () {
 var saveCustomer = function () {
     customerEdit_app.customer.gender = genderId.substring(6);
     console.log(addressProvinceId);
-    if (addressProvinceId != null) {
+    if(addressProvinceId!=null) {
         var tncAddress = {};
         var province = {};
         var city = {};
         var area = {};
-        var detail = $("#detail").val();
-        ;
+        var detail = $("#detail").val();;
         province.id = addressProvinceId.substring(8);
-        city.id = addressCityId.substring(4);
-        area.id = addressAreaId.substring(4);
-        tncAddress.province = province;
-        tncAddress.city = city;
-        tncAddress.area = area;
-        tncAddress.detail = detail;
+        city.id= addressCityId.substring(4);
+        area.id=addressAreaId.substring(4);
+        tncAddress.province=province;
+        tncAddress.city=city;
+        tncAddress.area=area;
+        tncAddress.detail=detail;
         customerEdit_app.customer.tncAddress = tncAddress;
     }
     console.log(customerEdit_app.customer);
@@ -132,24 +129,24 @@ var saveCustomer = function () {
         data: data,
         success: function (res) {
             if (res.code === 0) {
-                layer.msg("操作成功", {
-                    time: 1500
-                }, function () {
-                    window.location.href = '/tenancy/p/customerList'
+                layer.msg("操作成功",{
+                    time:1500
+                },function () {
+                    window.location.href='/tenancy/p/customerList'
                 });
-            } else {
+            }else {
                 handleAjax(res);
             }
         }
     })
 }
 
-var clickGender = function () {
-    var optionGender = "<option value='1'>男</option>" + "<option value='0'>女</option>"
+var  clickGender = function () {
+    var optionGender = "<option value='1'>男</option>"+"<option value='0'>女</option>"
     $("#gender").html(optionGender);
-    var ddGender = "<dd id='gender1' value='男'>男</dd>" + "<dd id='gender0' value='女'>女</dd>"
+    var ddGender = "<dd id='gender1' value='男'>男</dd>"+ "<dd id='gender0' value='女'>女</dd>"
     $($("#gender").siblings().eq(0).children("dl")[0]).html(ddGender);
-    $("#" + genderId).addClass('layui-this');
+    $("#"+ genderId).addClass('layui-this');
     $($("#gender").siblings().eq(0).children("dl").children("dd")).click(function () {
         /*添加选中样式*/
         $(this).siblings('dd').removeClass('layui-this');
@@ -163,9 +160,9 @@ var clickGender = function () {
 
 /*省市区已选项样式加载*/
 var loadStyle = function () {
-    $("#" + addressProvinceId).addClass('layui-this');
-    $("#" + addressCityId).addClass('layui-this');
-    $("#" + addressAreaId).addClass('layui-this');
+    $("#"+ addressProvinceId).addClass('layui-this');
+    $("#"+ addressCityId).addClass('layui-this');
+    $("#"+ addressAreaId).addClass('layui-this');
 }
 
 /*加载省内容及其点击事件，其他的就不多说了*/
@@ -173,112 +170,113 @@ var loadProvince = function (res) {
     /*因vue与lay冲突缘故，只能手动渲染*/
     var optionProvince = "<option value='0'>请选择省</option>"
     var ddProvince = "<dd id='province0' class='layui-select-tips'>请选择省</dd>"
-    for (var i = 0; i < res.data.length; i++) {
-        optionProvince += "<option value='" + res.data[i].id + "'>" + res.data[i].name + "</option>";
-        ddProvince += "<dd id=province" + res.data[i].id + " value='" + res.data[i].name + "'>" + res.data[i].name + "</dd>";
+    for(var i=0; i<res.data.length; i++) {
+        optionProvince += "<option value='"+res.data[i].id+"'>"+res.data[i].name+"</option>";
+        ddProvince += "<dd id=province"+res.data[i].id+" value='"+res.data[i].name+"'>"+res.data[i].name+"</dd>";
     }
     $("#province").html(optionProvince);
     /*怎么说呢？无法用言语来表达，结合layui的代码来写的*/
     /*提示框内容*/
-    $($("#province").siblings().eq(0).children("div").children("input")[0]).attr('placeholder', '请输入省');
+    $($("#province").siblings().eq(0).children("div").children("input")[0]).attr('placeholder','请输入省');
     /*添加省项*/
     $($("#province").siblings().eq(0).children("dl")[0]).html(ddProvince);
     loadStyle();
     /*点击事件*/
     $($("#province").siblings().eq(0).children("dl").children("dd")).click(function () {
         /*使市、区下拉框回复为默认状态*/
-        $($("#city").siblings().eq(0).children("div").children("input")[0]).attr('placeholder', '请输入市');
-        $($("#city").siblings().eq(0).children("div").children("input")[0]).attr('value', '');
-        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('placeholder', '请输入区（县）');
-        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('value', '');
+        $($("#city").siblings().eq(0).children("div").children("input")[0]).attr('placeholder','请输入市');
+        $($("#city").siblings().eq(0).children("div").children("input")[0]).attr('value','');
+        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('placeholder','请输入区（县）');
+        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('value','');
         /*添加选中样式*/
         $(this).siblings('dd').removeClass('layui-this');
         $(this).addClass('layui-this');
         /*获取选中的值将其显示在输入框里*/
         var provinceName = $(this)[0].getAttribute('value');
         addressProvinceId = $(this)[0].getAttribute('id');
-        $($("#province").siblings().eq(0).children("div").children("input")[0]).attr('value', provinceName);
+        $($("#province").siblings().eq(0).children("div").children("input")[0]).attr('value',provinceName);
         //console.log(this);
     });
 }
 
 /*字面意思，自己体会*/
 var clickCity = function () {
-    if (addressProvinceId != null) {
+    if(addressProvinceId!=null) {
         /*获取省id*/
         var parent_cityId = addressProvinceId.substring(8);
         /*获取市数据*/
-        loadAddress(parent_cityId, 1);
-    } else {
-        layer.msg("请选择省");
+        loadAddress(parent_cityId,1);
+    }else {
+        layer.mag("请选择省");
     }
+
 }
 
 var loadCity = function (res) {
     var optionCity = "<option value='0'>请选择市</option>"
     var ddCity = "<dd id='city0' class='layui-select-tips'>请选择市</dd>"
-    for (var i = 0; i < res.data.length; i++) {
-        optionCity += "<option value='" + res.data[i].id + "'>" + res.data[i].name + "</option>";
-        ddCity += "<dd id=city" + res.data[i].id + " value='" + res.data[i].name + "'>" + res.data[i].name + "</dd>";
+    for(var i=0; i<res.data.length; i++) {
+        optionCity += "<option value='"+res.data[i].id+"'>"+res.data[i].name+"</option>";
+        ddCity += "<dd id=city"+res.data[i].id+" value='"+res.data[i].name+"'>"+res.data[i].name+"</dd>";
     }
     $("#city").html(optionCity);
     /*怎么说呢？无法用言语来表达，结合layui的代码来写的*/
     /*提示框内容*/
-    $($("#city").siblings().eq(0).children("div").children("input")[0]).attr('placeholder', '请输入市');
+    $($("#city").siblings().eq(0).children("div").children("input")[0]).attr('placeholder','请输入市');
     /*添加省项*/
     $($("#city").siblings().eq(0).children("dl")[0]).html(ddCity);
     loadStyle();
     /*点击事件*/
     $($("#city").siblings().eq(0).children("dl").children("dd")).click(function () {
         /*使市、区下拉框回复为默认状态*/
-        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('placeholder', '请输入区（县）');
-        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('value', '');
+        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('placeholder','请输入区（县）');
+        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('value','');
         /*添加选中样式*/
         $(this).siblings('dd').removeClass('layui-this');
         $(this).addClass('layui-this');
         /*获取选中的值将其显示在输入框里*/
         var cityName = $(this)[0].getAttribute('value');
         addressCityId = $(this)[0].getAttribute('id');
-        $($("#city").siblings().eq(0).children("div").children("input")[0]).attr('value', cityName);
+        $($("#city").siblings().eq(0).children("div").children("input")[0]).attr('value',cityName);
         //console.log(this);
     });
 }
 
 var clickArea = function () {
-    if(addressCityId != null) {
+    if(addressCityId!=null) {
         var parent_AreaId = addressCityId.substring(4);
         loadAddress(parent_AreaId, 2);
-    }else {
-        layer.msg("请选择市");
+    } else {
+        layer.mag("请选择市");
     }
 }
 
 var loadArea = function (res) {
     var optionArea = "<option value='0'>请选择区（县）</option>"
     var ddArea = "<dd id='area0' class='layui-select-tips'>请选择区（县）</dd>"
-    for (var i = 0; i < res.data.length; i++) {
-        optionArea += "<option value='" + res.data[i].id + "'>" + res.data[i].name + "</option>";
-        ddArea += "<dd id=area" + res.data[i].id + " value='" + res.data[i].name + "'>" + res.data[i].name + "</dd>";
+    for(var i=0; i<res.data.length; i++) {
+        optionArea += "<option value='"+res.data[i].id+"'>"+res.data[i].name+"</option>";
+        ddArea += "<dd id=area"+res.data[i].id+" value='"+res.data[i].name+"'>"+res.data[i].name+"</dd>";
     }
     $("#area").html(optionArea);
     /*怎么说呢？无法用言语来表达，结合layui的代码来写的*/
     /*提示框内容*/
-    $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('placeholder', '请输入区（县）');
+    $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('placeholder','请输入区（县）');
     /*添加省项*/
     $($("#area").siblings().eq(0).children("dl")[0]).html(ddArea);
     loadStyle();
     /*点击事件*/
     $($("#area").siblings().eq(0).children("dl").children("dd")).click(function () {
         /*使市、区下拉框回复为默认状态*/
-        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('placeholder', '请输入区（县）');
-        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('value', '');
+        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('placeholder','请输入区（县）');
+        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('value','');
         /*添加选中样式*/
         $(this).siblings('dd').removeClass('layui-this');
         $(this).addClass('layui-this');
         /*获取选中的值将其显示在输入框里*/
         var areaName = $(this)[0].getAttribute('value');
         addressAreaId = $(this)[0].getAttribute('id');
-        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('value', areaName);
+        $($("#area").siblings().eq(0).children("div").children("input")[0]).attr('value',areaName);
         //console.log(this);
     });
 }
@@ -288,21 +286,21 @@ var loadAddress = function (aid, level) {
         type: "get",
         url: "/api/customer/address",
         data: {
-            aid: aid,
-            level: level
+            aid : aid,
+            level : level
         },
-        success: function (res) {
-            if (level === 0) {
+        success:function (res) {
+            if(level===0) {
                 loadProvince(res);
-            } else if (level === 1) {
+            }else if(level===1) {
                 loadCity(res);
-            } else {
+            }else {
                 loadArea(res);
             }
         }
     })
 }
 
-var back = function () {
+var back = function() {
     window.history.back();
 }
